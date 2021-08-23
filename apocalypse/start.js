344f4c8c8d428b4c5ccad0e862d7123ad49adab5
Malik Butler <malik.butler@gal.fl.gov>
2021-08-23 11:18:10 -0400

    fixed paths to test first old game

--- apocalypse/start.js
+++ apocalypse/start.js
@@ -2,12 +2,12 @@ var start = function(game){}
 
 start.prototype = {
   preload : function() {
-    this.load.image('bg', 'apocalypse/img/bg.png');
-    this.load.image('ground', 'apocalypse/img/ground.png');
-    this.load.image('fireball', 'apocalypse/img/fireball.png');
-    this.load.image('play', 'apocalypse/img/play.png');
-    this.load.image('gameover', 'apocalypse/img/gameover.png');
-    this.load.spritesheet('charr', 'apocalypse/img/charr.png', 30, 30);
+    this.load.image('bg', 'img/bg.png');
+    this.load.image('ground', 'img/ground.png');
+    this.load.image('fireball', 'img/fireball.png');
+    this.load.image('play', 'img/play.png');
+    this.load.image('gameover', 'img/gameover.png');
+    this.load.spritesheet('charr', 'img/charr.png', 30, 30);
   },
   create : function(){
     this.game.state.start("Splash");
